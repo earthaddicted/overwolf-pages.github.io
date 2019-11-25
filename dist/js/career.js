95a14a90dd666d57b837e1bc063df848b693af38
elenika.om <elenika.om@gmail.com>
2019-11-25 10:08:28 +0200

    Update career.js

--- dist/js/career.js
+++ dist/js/career.js
@@ -1,1 +1,72 @@
-"use strict";var comeetToken="1B16C4BD7005131B1A26F391B1",comeetID="B1.001",comeetJobsLink="https://www.comeet.co/careers-api/2.0/company/".concat(comeetID,"/positions?token=").concat(comeetToken,"&details=true");function insertJobsList(e){e.then((function(e){if(console.log("jobsList",e),e){var n=document.getElementById("jobs-block"),t=document.createDocumentFragment();e.forEach((function(e){var n=document.createElement("div");n.className="jobs__item",n.innerHTML=getJobTemplate(e),t.appendChild(n)})),n.appendChild(t)}}))}function fetchJobsList(){fetch(comeetJobsLink).then((function(e){e.ok&&insertJobsList(e.json())}))}function getJobTemplate(e){var n="";e.location&&"IL"===e.location.country&&(n="Israel");var t=e.location?e.location.city:"",o=t||n?'<span class="jobs__location">'.concat(t,", ").concat(n,"</span>"):"";e.employment_type=e.employment_type?e.employment_type:"Full-time",e.experience_level=e.experience_level?'<span class="jobs__level">'.concat(e.experience_level,"</span>"):"";var c=tagsFree(e.details[0].value);c=c&&c.length>300?c.substring(0,300)+"...":c;var a=e.name.replace(/\s+/g,"-").toLowerCase();return'<div class="jobs__item-top">\n                            <span class="jobs__item-title">\n                                '.concat(e.name,'\n                            </span>\n                        </div>\n                        <div class="jobs__item-container">\n                            <div class="jobs__info">\n                                ').concat(o,'\n                                <span class="jobs__type">\n                                    ').concat(e.employment_type,"\n                                </span>\n                                ").concat(e.experience_level,'\n                            </div>\n                            <p class="jobs__description">\n                                ').concat(c,'\n                            </p>\n                            <a href="career-inner_template.html?uid=').concat(e.uid,"&job=").concat(a,'" class="btn__transparent">\n                                View full details\n                            </a>\n                        </div>')}function tagsFree(e){var n=document.createElement("DIV");return n.innerHTML=e,n.textContent||n.innerText||""}fetchJobsList();
+const comeetToken = '1B16C4BD7005131B1A26F391B1';
+const comeetID = 'B1.001';
+const comeetJobsLink = `https://www.comeet.co/careers-api/2.0/company/${comeetID}/positions?token=${comeetToken}&details=true`;
+
+
+fetchJobsList();
+
+function insertJobsList(jobsJson) {
+    jobsJson.then(jobsList => {
+            console.log('jobsList', jobsList);
+            if(jobsList) {
+                const jobsBlockElement = document.getElementById('jobs-block');
+                const jobsElements = document.createDocumentFragment();
+                jobsList.forEach(job => {
+                    const jobElement = document.createElement('div');
+                    jobElement.className = 'jobs__item';
+                    jobElement.innerHTML = getJobTemplate(job);
+                    jobsElements.appendChild(jobElement);
+                });
+                jobsBlockElement.appendChild(jobsElements);
+            }
+        });
+}
+
+function fetchJobsList () {
+    fetch(comeetJobsLink)
+        .then(jobsResponse => {
+            if(jobsResponse.ok) {
+                insertJobsList(jobsResponse.json());
+            }
+        });
+}
+
+function getJobTemplate(job) {
+    let country = '';
+    if(job.location && job.location.country === 'IL') {
+        country = 'Israel';
+    }
+    const city = job.location ? job.location.city : '';
+    const locationElement = city || country ? `<span class="jobs__location">${city}, ${country}</span>` : '';
+    job.employment_type = job.employment_type ? job.employment_type : "Full-time";
+    job.experience_level = job.experience_level ? `<span class="jobs__level">${job.experience_level}</span>` : '';
+    let descrTagsFree = tagsFree(job.details[0].value);
+    descrTagsFree = descrTagsFree && descrTagsFree.length > 300 ? descrTagsFree.substring(0, 300) + '...' : descrTagsFree;
+    const formatedName = job.name.replace(/\s+/g, '-').toLowerCase();
+    return `<div class="jobs__item-top">
+                            <span class="jobs__item-title">
+                                ${job.name}
+                            </span>
+                        </div>
+                        <div class="jobs__item-container">
+                            <div class="jobs__info">
+                                ${locationElement}
+                                <span class="jobs__type">
+                                    ${job.employment_type}
+                                </span>
+                                ${job.experience_level}
+                            </div>
+                            <p class="jobs__description">
+                                ${descrTagsFree}
+                            </p>
+                            <a href="career-inner_template.html?uid=${job.uid}&job=${formatedName}" class="btn__transparent">
+                                View full details
+                            </a>
+                        </div>`
+}
+
+function tagsFree(html) {
+    const tmp = document.createElement("DIV");
+    tmp.innerHTML = html;
+    return tmp.textContent || tmp.innerText || "";
+}
